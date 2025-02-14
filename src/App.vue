4780aa9c4a5a5ee565a9b77e8d679d8b59c5504e
s-elo <zz321yy@qq.com>
2025-02-14 11:57:11 +0800

    feat: add splitpanes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { Splitpanes, Pane } from 'splitpanes';
+import 'splitpanes/dist/splitpanes.css';
 import MermaidPreview from './components/MermaidPreview.vue';
 
 const content = ref(`graph TB\na-->b`);
@@ -7,8 +9,32 @@ const content = ref(`graph TB\na-->b`);
 
 <template>
   <div class="container">
-    <MermaidPreview :content="content" />
+    <Splitpanes size="50">
+      <Pane min-size="25" max-size="100">
+        <div>text</div>
+      </Pane>
+      <Pane>
+        <MermaidPreview :content="content" />
+      </Pane>
+    </Splitpanes>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.container {
+  height: 100%;
+  padding: 20px;
+}
+</style>
+<style lang="scss">
+.splitpanes--vertical > .splitpanes__splitter {
+  width: 0.5rem;
+}
+.splitpanes__splitter {
+  background-color: #fafafa;
+  cursor: col-resize;
+  &:hover {
+    background-color: #e9e7e7;
+  }
+}
+</style>
